efa70b92f71d56e37b881458b9b121caabf541e4
irynasmile <73856579+irynasmile@users.noreply.github.com>
2021-06-15 15:40:35 +0200

    Local_storage FINISHED
    by Lady

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-
-
-import galleryItems from './gallery-items.js';
+import galleryItems from './gallery-items.js'
 // console.log(galleryItems);
 
-const galleryList = document.querySelector('.gallery');
-const openModal = document.querySelector('.js-lightbox');
-const openImageInModal = document.querySelector('.lightbox__image');
-const buttonCloseModal = document.querySelector('.lightbox__button');
+const galleryList = document.querySelector('.gallery')
+const openModal = document.querySelector('.js-lightbox')
+const openImageInModal = document.querySelector('.lightbox__image')
+const buttonCloseModal = document.querySelector('.lightbox__button')
+const btnWatchedMovies = document.querySelector('.add_watched')
+const btnWillWatchMovie = document.querySelector('.add_queue')
 
-const imagesMarkup = createCardsMarcup(galleryItems);
+// console.log(btnWatchedMovies);
+// console.log(btnWillWatchMovie);
+btnWatchedMovies.addEventListener(click, onBtnWatched)
+function onBtnWatched(e) {}
 
-galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
-galleryList.addEventListener('click', onGalleryListClick);
-buttonCloseModal.addEventListener('click', buttonCloseModalClick);
+btnWillWatchMovie.addEventListener('click', onBtnWillWatch)
+
+const imagesMarkup = createCardsMarcup('click', galleryItems)
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup)
+galleryList.addEventListener('click', onGalleryListClick)
+buttonCloseModal.addEventListener('click', buttonCloseModalClick)
 
 function createCardsMarcup(galleryItems) {
-  return galleryItems.map(({preview, original, description}) => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
       return `
 <li class="gallery__item">
    <a
@@ -31,30 +39,28 @@ function createCardsMarcup(galleryItems) {
    </a>
  </li>
       `
-  }).join('');
-};
+    })
+    .join('')
+}
 
-
-function onGalleryListClick(e) { 
-  e.preventDefault();
+function onGalleryListClick(e) {
+  e.preventDefault()
   if (e.target.classList.contains('js-gallery')) {
-    return;
+    return
   }
-  onOpenModalClick();
+  onOpenModalClick()
   updateModalImg(e.target.dataset.source, e.target.alt)
-};
+}
 
 function onOpenModalClick() {
-  openModal.classList.add('is-open');
-};
-function updateModalImg(src,alt  ) {
-    openImageInModal.src = src;
-    openImageInModal.alt = alt;
-
+  openModal.classList.add('is-open')
 }
- 
-function buttonCloseModalClick() {
-  openModal.classList.remove('is-open');
-  updateModalImg('','')
-};
+function updateModalImg(src, alt) {
+  openImageInModal.src = src
+  openImageInModal.alt = alt
+}
 
+function buttonCloseModalClick() {
+  openModal.classList.remove('is-open')
+  updateModalImg('', '')
+}
